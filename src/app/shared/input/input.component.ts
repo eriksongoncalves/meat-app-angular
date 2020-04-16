398b1614a997156f37d9e5212d45cfeddfe3db1e
Erikson Gonçalves <eriksongoncalves@yahoo.com.br>
2020-04-16 16:00:18 -0300

    feat: add FormBuilder

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -5,7 +5,7 @@ import {
   ContentChild,
   AfterContentInit,
 } from "@angular/core";
-import { NgModel } from "@angular/forms";
+import { NgModel, FormControlName } from "@angular/forms";
 
 @Component({
   selector: "mt-input-container",
@@ -17,17 +17,18 @@ export class InputComponent implements OnInit, AfterContentInit {
   @Input() errorMessage: string;
 
   @ContentChild(NgModel) model: NgModel;
+  @ContentChild(FormControlName) control: FormControlName;
 
   constructor() {}
 
   ngOnInit() {}
 
   ngAfterContentInit(): void {
-    this.input = this.model;
+    this.input = this.model || this.control;
 
     if (this.input === undefined) {
       throw new Error(
-        "Esse component precisa ser usado em uma diretiva NgModel"
+        "Esse component precisa ser usado em uma diretiva NgModel ou FormControlName"
       );
     }
   }
